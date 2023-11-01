6c462adc392b74ba903a8124b4aa22ff323ae5b1
DmitriyTR <skate1233@mail.ru>
2023-11-01 21:49:05 +0300

    Добавьте файлы проекта.

---
+++ HomeTask 6.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Soda
+{
+    private string flavor;
+
+    public Soda(string flavor = null)
+    {
+        this.flavor = flavor;
+    }
+
+    public string GetDescription()
+    {
+        if (!string.IsNullOrEmpty(flavor))
+        {
+            return $"У вас газировка с {flavor} вкусом";
+        }
+        else
+        {
+            return "У вас обычная газировка";
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Soda sodaWithFlavor = new Soda("клубничным");
+        Console.WriteLine(sodaWithFlavor.GetDescription()); // Вывод: "У вас газировка с клубничным вкусом"
+
+        Soda plainSoda = new Soda();
+        Console.WriteLine(plainSoda.GetDescription()); // Вывод: "У вас обычная газировка"
+    }
+}
